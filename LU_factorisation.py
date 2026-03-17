5eb52892904e3ff80c52b744a8d2e2b1f0b59dd4
Henry Wang <hksq7527@leeds.ac.uk>
2026-03-17 20:01:14 +0000

    Huh

--- LU_factorisation.py
+++ LU_factorisation.py
@@ -1,3 +1,33 @@
+import numpy as np
+from scipy.sparse import diags
+
+
+def generate_safe_system(n):
+    """
+    Generate a linear system A x = b where A is strictly diagonally dominant,
+    ensuring LU factorization without pivoting will work.
+
+    Parameters:
+        n (int): Size of the system (n x n)
+
+    Returns:
+        A (ndarray): n x n strictly diagonally dominant matrix
+        b (ndarray): RHS vector
+        x_true (ndarray): The true solution vector
+    """
+
+    k = [np.ones(n - 1), -2 * np.ones(n), np.ones(n - 1)]
+    offset = [-1, 0, 1]
+    A = diags(k, offset).toarray()
+
+    # Solution is always all ones
+    x_true = np.ones((n, 1))
+
+    # Compute b = A @ x_true
+    b = A @ x_true
+
+    return A, b, x_true
+
 def lu_factorisation(A):
     """
     Compute the LU factorisation of a square matrix A.
